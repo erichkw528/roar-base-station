330119b558fa4146e36311419b6a26f5ac84df98
Michael Wu <wuxiaohua1011@gmail.com>
2023-08-23 01:00:47 -0700

    lite: default config added

--- launch/main.launch.py
+++ launch/main.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     rosbridge_launch_path: Path = Path(get_package_share_directory("rosbridge_server")) / "launch" / "rosbridge_websocket_launch.xml"
     assert rosbridge_launch_path.exists(), f"{rosbridge_launch_path} does not exist"
     rosbridge_launch = IncludeLaunchDescription(
-        AnyLaunchDescriptionSource(rosbridge_launch_path), 
+        AnyLaunchDescriptionSource(rosbridge_launch_path.as_posix()), 
         launch_arguments={"port": "9090"}.items())
     ld.add_action(rosbridge_launch)
 
